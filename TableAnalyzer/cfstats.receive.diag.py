38a54a41b116e46434c158f468f0e2482be184a0
privatebrycehao <brycehao@gmail.com>
2019-12-15 21:51:12 -0500

    select all option added
    select all or from yml file

--- TableAnalyzer/cfstats.receive.diag.py
+++ TableAnalyzer/cfstats.receive.diag.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 import config
 import util
+import os
 
 parser = argparse.ArgumentParser(
     description='Collecting config varibales from environments.yaml and Start receiving stats',
@@ -10,19 +11,28 @@ parser.add_argument('region', type=str, help='Region {us-east-1|usw|us|aws}')
 parser.add_argument('environ', type=str, help='Environment {dev|stage|prod}')
 parser.add_argument('db', type=str, help='Database {cassandra|spark}')
 parser.add_argument('location', type=str, help= 'Locations {name}')
-parser.add_argument('debug', type=int, help='Debuger {0|1}')
+parser.add_argument('get_all', type=bool, help='if get all data True|False')
+parser.add_argument('version', type=int, help='version {2|3}')
+parser.add_argument('debug', type=int, help='debugger {0|1}')
 args = parser.parse_args()
 
 
 def main():
     location = args.location
     region = args.region
-    hosts = config.get_keys(args.region, args.environ, args.db)
+    if args.get_all:
+        file = str(location + '/nodes')
+        hosts = os.listdir(file)
+    else:
+        hosts = config.get_keys(args.region, args.environ, args.db)
+    stats = str('cfstats')
+    if args.version is 3:
+        stats = str('tablestats')
     if len(hosts) > 1:
         for i, host in enumerate(hosts):
             if args.debug: print("Processing files", (i + 1))
             store_path = str('data' + '/' + region + '/' + args.environ + "/" + host + '.txt')
-            path = str(location+ '/nodes/' + host + "/nodetool/cfstats")
+            path = str(location+ '/nodes/' + host + '/nodetool/'+stats)
             fw = open(store_path, 'w')
             for line in open(path):
                 fw.write(line.rstrip("\n"))
@@ -34,7 +44,7 @@ def main():
     elif len(hosts) == 1:
         if args.debug: print("Processing file")
         store_path = str('data' + '/' + region + '/' + args.environ + "/" + hosts[0] + '.txt')
-        path = str(location + '/nodes/' + hosts[0] + "/nodetool/cfstats")
+        path = str(location + '/nodes/' + hosts[0] + "/nodetool/"+stats)
         fw = open(store_path, 'w')
         for line in open(path):
             fw.write(line.rstrip("\n"))
