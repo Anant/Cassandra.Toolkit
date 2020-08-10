2f1b4b7cee849ab8b53a62f2d9276fc3a9448d12
ameerajmal111 <ameerajmal.tx@gmail.com>
2020-08-10 14:39:51 -0500

    Add files via upload
    added formatting to few more columns

--- TableAnalyzer/csv2formattedxls.py
+++ TableAnalyzer/csv2formattedxls.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python2
 import pandas as pd
 import xlsxwriter
 import csv
@@ -35,21 +36,50 @@ worksheet.set_column('A:A', None, None, {'hidden': 1})
 # Apply a conditional format to the cell range.
 
 # Read / Write
-worksheet.conditional_format('E2:F{0}'.format(row_count), {'type': 'data_bar'})
+worksheet.conditional_format('E2:E{0}'.format(row_count), {'type': 'data_bar'})
 worksheet.conditional_format('F2:F{0}'.format(row_count), {'type': 'data_bar'})
 
 # Space used 
 worksheet.conditional_format('G2:G{0}'.format(row_count), {'type': 'data_bar'})
 worksheet.conditional_format('H2:H{0}'.format(row_count), {'type': 'data_bar'})
 
-# Partition Sizes
-worksheet.conditional_format('M2:M{0}'.format(row_count), {'type': '3_color_scale', 'min_color': 'green', 'min_value': 0, 'max_color': 'red'})
-worksheet.conditional_format('N2:N{0}'.format(row_count), {'type': '3_color_scale', 'min_color': 'green', 'min_value': 0, 'max_color': 'red'})
-worksheet.conditional_format('O2:O{0}'.format(row_count), {'type': '3_color_scale', 'min_color': 'green', 'min_value': 0, 'max_color': 'red'})
+#Offheapmemoryusedtotal of V #3
+worksheet.conditional_format('I2:I{0}'.format(row_count), {'type': 'data_bar'})
 
+# Partition Sizes
 worksheet.conditional_format('K2:K{0}'.format(row_count), {'type': '2_color_scale', 'min_color': '#FFFFFF', 'min_value': 0, 'max_color': 'red'})
 worksheet.conditional_format('R2:R{0}'.format(row_count), {'type': '2_color_scale', 'min_color': '#FFFFFF', 'min_value': 0, 'max_color': 'red'})
 
+# Numberofpartitionsestimate	
+# Memtablecellcount	
+worksheet.conditional_format('L2:L{0}'.format(row_count), {'type': '2_color_scale', 'min_color': '#FFFFFF', 'min_value': 0, 'max_color': 'red'})
+# Memtabledatasize	
+worksheet.conditional_format('M2:M{0}'.format(row_count), {'type': '3_color_scale', 'min_color': 'green', 'min_value': 0, 'max_color': 'red'})
+# Memtableoffheapmemoryused
+worksheet.conditional_format('N2:N{0}'.format(row_count), {'type': '3_color_scale', 'min_color': 'green', 'min_value': 0, 'max_color': 'red'})
+# Memtableswitchcount	
+worksheet.conditional_format('O2:O{0}'.format(row_count), {'type': '3_color_scale', 'min_color': 'green', 'min_value': 0, 'max_color': 'red'})
+# Localreadcount	
+worksheet.conditional_format('P2:P{0}'.format(row_count), {'type': 'data_bar'})
+# Localreadlatency	
+# Localwritecount	
+# Localwritelatency	
+# Pendingflushes	
+# Percentrepaired	
+# Bloomfilterfalsepositives	
+# Bloomfilterfalseratio	
+# Bloomfilterspaceused	
+# Bloomfilteroffheapmemoryused	
+# Indexsummaryoffheapmemoryused	
+# Compressionmetadataoffheapmemoryused	
+# Compactedpartitionminimumbytes	
+# Compactedpartitionmaximumbytes	
+# Compactedpartitionmeanbytes	
+# Averagelivecellsperslicelastfiveminutes	
+# Maximumlivecellsperslicelastfiveminutes	
+# Averagetombstonesperslicelastfiveminutes	
+# Maximumtombstonesperslicelastfiveminutes	
+# DroppedMutations
 # Close the Pandas Excel writer and output the Excel file.
 writer.save()
 
